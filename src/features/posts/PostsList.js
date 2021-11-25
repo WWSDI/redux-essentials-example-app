714d098436a47a2b524cfa8ab9f137b24f1fe5d5
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-11-25 17:53:29 +1300

    feat: edit an existing post feature

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 // - Our posts list read the initial set of posts from the store with useSelector and rendered the initial UI
 // - We dispatched the postAdded action containing the data for the new post entry
@@ -15,6 +16,9 @@ export const PostsList = () => {
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
     </article>
   ))
 
